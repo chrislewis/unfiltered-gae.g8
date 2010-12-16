05563b20689a07e4c7aa3ac5b244eb8898d6acc0
chrislewis <chris@thegodcode.net>
2010-12-16 01:24:17 -0500

    unfiltered 0.2.3

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends AppengineProject(info) {
-  val uf_version = "0.2.2"
+  val uf_version = "0.2.3"
   
   val lift_json = "net.liftweb" %% "lift-json" % "2.2-M1" 
   
