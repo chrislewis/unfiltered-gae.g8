4f1a0a10b86a2fe03b98f237717677c9fd045f6c
chrislewis <chris@thegodcode.net>
2010-12-15 20:56:09 -0500

    deps

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,5 +1,16 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" %"1.6.5" % "test"
+class Project(info: ProjectInfo) extends AppengineProject(info) {
+  val uf_version = "0.2.2"
+  
+  val lift_json = "net.liftweb" %% "lift-json" % "2.2-M1" 
+  
+  // unfiltered
+  lazy val uff = "net.databinder" %% "unfiltered-filter" % uf_version
+  lazy val ufj = "net.databinder" %% "unfiltered-jetty" % uf_version
+  lazy val ufjs = "net.databinder" %% "unfiltered-json" % uf_version
+
+  // testing
+  lazy val uf_spec = "net.databinder" %% "unfiltered-spec" % uf_version % "test"
+  lazy val specs = "org.scala-tools.testing" %% "specs" %"1.6.5" % "test"
 }
